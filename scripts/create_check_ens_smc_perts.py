362e8f4e5ef447c31ba3489aaf294b1f64120ad0
elliottwarren <e.l.warren@pgr.reading.ac.uk>
2019-11-21 16:49:14 +0000

    init commit

---
+++ scripts/create_check_ens_smc_perts.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+
+"""
+Create perturbations from the ensemble mean soil moisture content (SMC), check their scientifically valid (not
+beyond reasonable limits), and append them to the SURF files.
+
+Created by Elliott Warren Thurs 21th Nov 2019: elliott.warren@metoffice.gov.uk
+"""
+
