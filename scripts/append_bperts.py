d18f11d6c1eefbf5ee8b223786c38ab520525ce3
elliottwarren <e.l.warren@pgr.reading.ac.uk>
2019-12-11 14:48:40 +0000

    Code can work if the EKF file is missing. In this case, breeding pert is loaded, scaled and saved.

--- scripts/append_bperts.py
+++ scripts/append_bperts.py
@@ -19,7 +19,6 @@ Testing (including soil temperature) carried out in:
 
 import os
 import shutil
-import numpy as np
 
 import mule
 import mule.operators
@@ -55,10 +54,10 @@ if ROSE_DATACPT6H is None:
     ENS_MEMBER = '1'
     ENS_SOIL_BPERT_FILEPATH = '/data/users/ewarren/R2O_projects/soil_moisture_pertubation/data/20181201T0600Z/' \
                    'engl_smc/engl_smc_bpert/engl_smc_bpert_{0:03d}'.format(int(ENS_MEMBER))
-    # ENS_SOIL_EKF_FILEPATH = '/data/users/ewarren/R2O_projects/soil_moisture_pertubation/data/20181201T1200Z/engl_um/'\
-    #                         'engl_surf_inc' - Breo's change
-    ENS_SOIL_EKF_FILEPATH = '/data/users/ewarren/R2O_projects/soil_moisture_pertubation/data/20181201T1200Z/engl_smc/' \
-                            'engl_smc_p{0:04d}'.format(int(ENS_MEMBER))
+    ENS_SOIL_EKF_FILEPATH = '/data/users/ewarren/R2O_projects/soil_moisture_pertubation/data/20181201T1200Z/engl_um/'\
+                            'engl_um_{0:03d}/engl_surf_inc'.format(int(ENS_MEMBER))  # Breo's change
+    # ENS_SOIL_EKF_FILEPATH = '/data/users/ewarren/R2O_projects/soil_moisture_pertubation/data/20181201T1200Z/engl_smc/' \
+    #                         'engl_smc_p{0:04d}'.format(int(ENS_MEMBER))  # before Breo's change
     TUNING_FACTOR = '1'
     OVERWRITE_PERT_FILES = False
 
@@ -67,8 +66,8 @@ ENS_MEMBER_INT = int(ENS_MEMBER)
 TUNING_FACTOR_FLOAT = float(TUNING_FACTOR)
 
 # quick check TUNING_FACTOR is not less than 0
-if TUNING_FACTOR_FLOAT < 0.0:
-    raise ValueError('TUNING_FACTOR value of {0} is invalid. MUst be set >0.0'.format(TUNING_FACTOR_FLOAT))
+if TUNING_FACTOR_FLOAT <= 0.0:
+    raise ValueError('TUNING_FACTOR value of {0} is invalid. MUst be set >= 0.0'.format(TUNING_FACTOR_FLOAT))
 
 # ------------------------------------
 
@@ -127,70 +126,85 @@ class CachingOperator(mule.DataOperator):
             src_field._data = src_field.get_data()
         return src_field._data
 
+# load and process breeding functions
 
-def mem_to_str(member):
+
+def load_prev_ens_bpert_data():
+
     """
-    Converts an ensemble member input number into the filename string used to locate files
-    :param member: (int or char)
-    :return: mem_str: (str) 3 char string with leading zeros e.g. 001
+    Loads in previous cycle ensemble breeding perturbation data. Sorted into a dict structure by field, level to
+    produce a list containing the data for ensemble members.
+    E.g. {9: {1: [field1], 2: [field2], ...}} where 9 = STASH code and 1 and 2 are levels
+
+    Also contains the land_sea mask (STASH = 30), required by the soil moisture content field
+    :return: bpert_data_in (dictionary) breeding perturbation data
+    :return: ff_file_in (object) bpert file
     """
 
-    fail_str = 'Cannot convert ensemble member input to filename string: {0}'.format(member)
-    if isinstance(member, str):
-        if member.isdigit():
-            mem_str = '{0:03d}'.format(int(member))
+    # load in the required data for each file:
+    # for member in MEMBERS_PERT_INTS:
+
+    # set up the returned data structure...
+    bpert_data_in = {}
+    for stash in STASH_TO_LOAD:
+        if stash in MULTI_LEVEL_STASH:
+            bpert_data_in[stash] = {}
         else:
-            raise ValueError(fail_str)
-    elif isinstance(member, int):
-        mem_str = '{0:03d}'.format(member)
-    else:
-        raise ValueError(fail_str)
-    return mem_str
+            bpert_data_in[stash] = []
+
+    # data file to open:
+    ff_file_in = mule.load_umfile(ENS_SOIL_BPERT_FILEPATH)
+    #ff_file_in = mule.DumpFile.from_file(ENS_SOIL_BPERT_FILEPATH)
+    ff_file_in.remove_empty_lookups()
+
+    # pull out the fields:
+    for field in ff_file_in.fields:
+        # is this SMC or land sea mask?
+        if field.lbuser4 in STASH_TO_LOAD:
+            if field.lbuser4 in MULTI_LEVEL_STASH:
+                # multi-level fields are a dict, with a list for each level
+                if field.lblev in bpert_data_in[field.lbuser4].keys():
+                    bpert_data_in[field.lbuser4][field.lblev] = field
+                else:
+                    bpert_data_in[field.lbuser4][field.lblev] = field
+            else:
+                # single level fields are a flat list:
+                bpert_data_in[field.lbuser4] = field
+
+    return bpert_data_in, ff_file_in
 
 
-def set_time_metadata(data_field, time_template_field):
+def load_scale_bpert():
+
     """
-    Sets the time metadata of an input data field to that of a template field
-    :param data_field: (field) data field that needs its time changed
-    :param time_template_field: (field) template field that has the time we want to change to
+    Load and scale the breeding perturbations
     :return:
     """
 
-    data_field.lbyr = time_template_field.lbyr
-    data_field.lbmon = time_template_field.lbmon
-    data_field.lbdat = time_template_field.lbdat
-    data_field.lbhr = time_template_field.lbhr
-    data_field.lbmin = time_template_field.lbmin
+    # load in the breeding perturbation file, for this member, from the last cycle
+    # includes the land-sea mask
+    bpert_fields, ff_file_in = load_prev_ens_bpert_data()
+    landsea_field = bpert_fields[STASH_LAND_SEA_MASK]
 
-    data_field.lbyrd = time_template_field.lbyrd
-    data_field.lbmond = time_template_field.lbmond
-    data_field.lbdatd = time_template_field.lbdatd
-    data_field.lbhrd = time_template_field.lbhrd
-    data_field.lbmind = time_template_field.lbmind
+    # Combined perturbation from breeding method (bpert) with the EKF perturbation, for each soil level.
+    # Use TUNING_FACTOR to scale the amount of bpert to add (scaling applied equally to all levels).
+    tuning_factor_scaling = mule.operators.ScaleFactorOperator(TUNING_FACTOR_FLOAT)
 
-    data_field.lbtim = time_template_field.lbtim
-    data_field.lbft = time_template_field.lbft
+    bpert_scaled = {stash: {} for stash in STASH_TO_MAKE_PERTS}
+    for stash in STASH_TO_MAKE_PERTS:
+        for level in bpert_fields[stash].iterkeys():
+            bpert_l = bpert_fields[stash][level]
+            bpert_scaled[stash][level] = tuning_factor_scaling(bpert_l)
 
-    # field may not have lbsec or lbsecd - so if not, set them to 0
-    if not hasattr(time_template_field, 'lbsec'):
-        data_field.lbsec = 0
-    else:
-        data_field.lbsec = time_template_field.lbsec
-
-    if not hasattr(time_template_field, 'lbsecd'):
-        data_field.lbsecd = 0
-    else:
-        data_field.lbsecd = time_template_field.lbsec
-
-    return
+    return bpert_scaled, landsea_field
 
 
-# loading functions
+# load and process EKF functions
 
-def load_soil_data(stash_and_constraints, cache=False):
+
+def load_soil_EFK_data(stash_and_constraints, cache=False):
     """
-    Loads required data from a single file, supplied as a file name to load
-    or a ff object, into a simple dictionary.
+    Loads EKF soil data
 
     Returns the requested FieldsFile data, and the FieldsFile object
     :param stash_and_constraints: (dictionary) stash codes and additional constraints to load
@@ -253,55 +267,10 @@ def load_soil_data(stash_and_constraints, cache=False):
     return ff_data, ff_obj
 
 
-def load_prev_ens_bpert_data():
+def load_ekf_combine_with_bpert(bpert_scaled):
 
     """
-    Loads in previous cycle ensemble breeding perturbation data. Sorted into a dict structure by field, level to
-    produce a list containing the data for ensemble members.
-    E.g. {9: {1: [field1], 2: [field2], ...}} where 9 = STASH code and 1 and 2 are levels
-
-    Also contains the land_sea mask (STASH = 30), required by the soil moisture content field
-    :return: bpert_data_in (dictionary) breeding perturbation data
-    :return: ff_file_in (object) bpert file
-    """
-
-    # load in the required data for each file:
-    # for member in MEMBERS_PERT_INTS:
-
-    # set up the returned data structure...
-    bpert_data_in = {}
-    for stash in STASH_TO_LOAD:
-        if stash in MULTI_LEVEL_STASH:
-            bpert_data_in[stash] = {}
-        else:
-            bpert_data_in[stash] = []
-
-    # data file to open:
-    ff_file_in = mule.DumpFile.from_file(ENS_SOIL_BPERT_FILEPATH)
-    ff_file_in.remove_empty_lookups()
-
-    # pull out the fields:
-    for field in ff_file_in.fields:
-        # is this the SMC?
-        if field.lbuser4 in STASH_TO_LOAD:
-            if field.lbuser4 in MULTI_LEVEL_STASH:
-                # multi-level fields are a dict, with a list for each level
-                if field.lblev in bpert_data_in[field.lbuser4].keys():
-                    bpert_data_in[field.lbuser4][field.lblev] = field
-                else:
-                    bpert_data_in[field.lbuser4][field.lblev] = field
-            else:
-                # single level fields are a flat list:
-                bpert_data_in[field.lbuser4] = field
-
-    return bpert_data_in, ff_file_in
-
-
-def load_combine_ekf_bpert():
-
-    """
-    Load in the EKF and breeding perturbations, and combine them. Also load in the land-sea mask
-    from the breeding perturbations file.
+    Load in EFK soil perturbations and combine with the already scaled breeding perturbations
 
     :return: soil_comb_pert: (dictionary) combined perturbations of the EKF and breeding scheme
     :return: bpert_landsea_field (field) land sea mask from the breeding perturbations file
@@ -314,45 +283,64 @@ def load_combine_ekf_bpert():
                        STASH_LAND_SEA_MASK: None,
                        STASH_LANDFRAC: {'lbuser5': [PSEUDO_LEVEL_LANDICE]}}
 
-    # load smc data in (current cycle analysis)
-    soil_fields, smc_ff = load_soil_data(stash_from_dump, cache=True)
-
-    # load in the breeding perturbation file, for this member, from the last cycle
-    # includes the land-sea mask
-    bpert_fields, ff_file_in = load_prev_ens_bpert_data()
-    landsea_field = bpert_fields[STASH_LAND_SEA_MASK]
+    # load in the EKF and other
+    soil_fields, smc_ff = load_soil_EFK_data(stash_from_dump, cache=True)
 
     # Combined perturbation from breeding method (bpert) with the EKF perturbation, for each soil level.
-    # Use TUNING_FACTOR to scale the amount of bpert to add (scaling applied equally to all levels).
-    tuning_factor_scaling = mule.operators.ScaleFactorOperator(TUNING_FACTOR_FLOAT)
     adder = mule.operators.AddFieldsOperator(preserve_mdi=True)
 
-    soil_comb_pert = {stash: {} for stash in STASH_TO_MAKE_PERTS}
+    soil_total_pert = {stash: {} for stash in STASH_TO_MAKE_PERTS}
     for stash in STASH_TO_MAKE_PERTS:
-        for level in bpert_fields[stash].iterkeys():
-            bpert_l = bpert_fields[stash][level]
-            bpert_l_scaled = tuning_factor_scaling(bpert_l)
+        for level, bpert_l in bpert_scaled[stash].iteritems():
             ekf_pert_l = soil_fields[stash][level]
-            soil_comb_pert[stash][level] = adder([bpert_l_scaled, ekf_pert_l])
+            soil_total_pert[stash][level] = adder([bpert_l, ekf_pert_l])
 
-    return soil_comb_pert, landsea_field
+    return soil_total_pert
 
 # save
 
 
-def save_comb_pert(soil_pert, landsea_field):
+def save_total_pert(soil_pert, landsea_field, template_file=ENS_SOIL_EKF_FILEPATH):
 
-    # now write out the perturbation on top of the fields in the actual perturbation file.
-    output_pert_file = ENS_SOIL_EKF_FILEPATH
-    tmp_output_pert_file = output_pert_file + '_tmp.ff'
+    """
+    Save the total perturbation ready for the IAU to ingest. Use the EKF file as a template, if present,
+    as this is already intended for the IAU and to help future-proof the process as the file may
+    include more information in the future.
 
-    pert_ff_in = mule.AncilFile.from_file(output_pert_file, remove_empty_lookups=True)  # the full existing file
-    pert_ff_out = pert_ff_in.copy(include_fields=False)  # empty copy
+    :param soil_pert: (dictionary) total soil perturabtion
+    :param landsea_field: (field) land sea mask from the breeding perturbation file
+    :return:
+    """
 
-    # Remove the level_dependent_constants fixed header if present, as it is no longer needed in an ancillary file and
-    # mule will not save file with it in.
-    if hasattr(pert_ff_out, 'level_dependent_constants'):
-        pert_ff_out.level_dependent_constants = None
+    def ensure_fields_file():
+        # Set the dataset_type to 3 (fields file) encase the file type isn't already
+        pert_ff_out.fixed_length_header.dataset_type = 3
+        return
+
+    def ensure_ancil_file():
+        # If file is an ancillary (dataset_type = 4) and has level_dependent_constants as a header...
+        # Remove the level_dependent_constants fixed header, as it should not exist in an ancillary file and
+        # mule will not save file with it in.
+        if (pert_ff_in.fixed_length_header.dataset_type == 4) & hasattr(pert_ff_out, 'level_dependent_constants'):
+            pert_ff_out.level_dependent_constants = None
+        return
+
+    # Ideally use the existing EKF file as a template
+    if os.path.exists(ENS_SOIL_EKF_FILEPATH):
+        output_pert_file = ENS_SOIL_EKF_FILEPATH
+        tmp_output_pert_file = output_pert_file + '_tmp.ff'
+        pert_ff_in = mule.AncilFile.from_file(output_pert_file, remove_empty_lookups=True)
+        pert_ff_out = pert_ff_in.copy(include_fields=False)  # empty copy
+        # EKF file is an ancillary that can contain bad headers, so ensure it is suitable for saving as an ancillary.
+        ensure_ancil_file()
+    else:
+        # Use the existing breeding perturbation file as a template
+        output_pert_file = ENS_SOIL_BPERT_FILEPATH
+        tmp_output_pert_file = output_pert_file + '_tmp.ff'
+        pert_ff_in = mule.FieldsFile.from_file(output_pert_file, remove_empty_lookups=True)
+        pert_ff_out = pert_ff_in.copy(include_fields=False)  # empty copy
+        # breeding perturbation file is a dump file, so convert it to a fields file for saving
+        ensure_fields_file()
 
     # now go through the fields in the pert_ff_in and as long as they are not duplicates
     # of the fields in the this_perts object, add them to pert_ff_out.
@@ -369,8 +357,6 @@ def save_comb_pert(soil_pert, landsea_field):
             pass  # don't load in the existing perturbed data. We want to replace this!
         elif field.lbuser4 == STASH_LAND_SEA_MASK:
             out_pert_has_lsm = True
-            if not np.all(field.get_data() == soil_pert[STASH_LAND_SEA_MASK].get_data()):
-                raise ValueError('Inconsistent land sea masks in perturbation files')
             pert_ff_out.fields.append(field)
         else:
             pert_ff_out.fields.append(field)
@@ -387,15 +373,11 @@ def save_comb_pert(soil_pert, landsea_field):
         if stash in MULTI_LEVEL_STASH:
             # write out each level (the order now matters!):
             for level in sorted(soil_pert[stash]):
-                # # modify the headers for time:
-                set_time_metadata(soil_pert[stash][level], template_time_field)
-                # and append:
                 pert_ff_out.fields.append(soil_pert[stash][level])
         else:
-            set_time_metadata(soil_comb_pert[stash], template_time_field)
             pert_ff_out.fields.append(soil_pert[stash])
 
-    # pert_ff_out.validate
+    # pert_ff_out.validate to check validity
     pert_ff_out.to_file(tmp_output_pert_file)
 
     # if we are done, and this id being run 'for real'
@@ -410,10 +392,20 @@ if __name__ == '__main__':
 
     # load the breeding perturbation data, EFK data and combine them.
     # Also load the land-sea mask for saving the data later.
-    soil_comb_pert, bpert_landsea_field = load_combine_ekf_bpert()
+    # soil_comb_pert, bpert_landsea_field = load_combine_ekf_bpert()
 
-    # save combined perturbations in the original perturbation file
-    save_comb_pert(soil_comb_pert, bpert_landsea_field)
+    # load and scale the breeding perturbation data based on the TUNING_FACTOR_INT value
+    bpert_scaled, bpert_landsea_field = load_scale_bpert()
+
+    # if the EKF file exists, read it in and combine with the scaled breeding perturbation, to create a total.
+    # Else take the breeding perturbation data alone as the 'total'.
+    if os.path.exists(ENS_SOIL_EKF_FILEPATH):
+        total_pert = load_ekf_combine_with_bpert(bpert_scaled)
+    else:
+        total_pert = bpert_scaled
+
+    # save total perturbations in the original perturbation file
+    save_total_pert(total_pert, bpert_landsea_field)
 
     exit(0)
 
